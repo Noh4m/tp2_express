1aebae4acd358f6556bf26f6a6663da5af1ae2c6
ibrahima-eemi <148555822+ibrahima-eemi@users.noreply.github.com>
2024-03-01 18:09:02 +0100

    Update personnagesRoutes.js

--- personnagesRoutes.js
+++ personnagesRoutes.js
@@ -1,8 +1,6 @@
-// ./routes/myRouter.js
 const express = require('express');
 const router = express.Router();
 
-// Exemple de route
 router.get('/', (req, res) => {
   res.send('Page d\'accueil');
 });
